e4945403d0ad3771f69bee4fa9959242193df502
pedrorhamon <pedrorhamon16@gmail.com>
2021-09-19 16:08:03 -0300

    alterações significativas no main

--- src/main/java/com/kingstar/curso/jpa/ConsultaCozinhaMain.java
+++ src/main/java/com/kingstar/curso/jpa/ConsultaCozinhaMain.java
@@ -1,5 +1,27 @@
 package com.kingstar.curso.jpa;
 
-public class ConsultaCozinhaMain {
+import java.util.List;
 
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.kingstar.curso.CursoApiApplication;
+import com.kingstar.curso.domain.entity.Cozinha;
+
+public class ConsultaCozinhaMain {
+	
+	public static void main(String[] args) {
+		ApplicationContext appC = new SpringApplicationBuilder(CursoApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		CadastroCozinha cadastroCozinha = appC.getBean(CadastroCozinha.class);
+		
+		List<Cozinha> cozinhas = cadastroCozinha.listar();
+		
+		for(Cozinha cozinha: cozinhas) {
+			System.out.println(cozinha.getNome());
+		}
+	}
 }
