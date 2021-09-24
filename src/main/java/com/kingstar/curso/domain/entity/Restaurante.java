3c7e1bee1a372d46f0c7a24949f9c4f01f990034
pedrorhamon <pedrorhamon16@gmail.com>
2021-09-23 23:29:35 -0300

    Usando lombok

--- src/main/java/com/kingstar/curso/domain/entity/Restaurante.java
+++ src/main/java/com/kingstar/curso/domain/entity/Restaurante.java
@@ -6,63 +6,19 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Restaurante {
-	
+
 	@Id
 	private Long Id;
-	
+
 	private String nome;
-	
+
 	@Column(name = "taxa_frente")
 	private BigDecimal taxaFrente;
-
-	public Long getId() {
-		return Id;
-	}
-
-	public void setId(Long id) {
-		Id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public BigDecimal getTaxaFrente() {
-		return taxaFrente;
-	}
-
-	public void setTaxaFrente(BigDecimal taxaFrente) {
-		this.taxaFrente = taxaFrente;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Restaurante other = (Restaurante) obj;
-		if (Id == null) {
-			if (other.Id != null)
-				return false;
-		} else if (!Id.equals(other.Id))
-			return false;
-		return true;
-	}
 }
