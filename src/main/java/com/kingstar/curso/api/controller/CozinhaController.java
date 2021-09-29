af8e4becb32fd7357667842d7898efc5b3901eca
pedrorhamon <pedrorhamon16@gmail.com>
2021-09-29 20:18:43 -0300

    getMapping junto com a lista

---
+++ src/main/java/com/kingstar/curso/api/controller/CozinhaController.java
@@ -0,0 +1,25 @@
+package com.kingstar.curso.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kingstar.curso.domain.entity.Cozinha;
+import com.kingstar.curso.domain.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+	
+	@GetMapping
+	public List<Cozinha> listar(){
+		return cozinhaRepository.listar();
+	}
+
+}
