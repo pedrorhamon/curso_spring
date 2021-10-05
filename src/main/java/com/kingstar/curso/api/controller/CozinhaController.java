cec9d6da4dad6724367fd94eeb4daf9dfba6b86d
pedrorhamon <pedrorhamon16@gmail.com>
2021-10-04 21:03:41 -0300

    consulta singleton resourse com get

--- src/main/java/com/kingstar/curso/api/controller/CozinhaController.java
+++ src/main/java/com/kingstar/curso/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.kingstar.curso.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,20 @@ import com.kingstar.curso.domain.entity.Cozinha;
 import com.kingstar.curso.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(value = "/cozinhas")
 public class CozinhaController {
 	
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-	@GetMapping
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Cozinha> listar(){
 		return cozinhaRepository.listar();
+	}
+	
+	
+	public Cozinha buscar(Long id) {
+		return cozinhaRepository.buscar(id);
 	}
 
 }
