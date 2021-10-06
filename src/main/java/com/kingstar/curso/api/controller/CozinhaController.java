dcb9cbc39088ce778bee56ab2b20b5f15b2a269f
pedrorhamon <pedrorhamon16@gmail.com>
2021-10-06 00:01:55 -0300

    Customizando a representação em XML com Wrapper e anotações do Jackson

--- src/main/java/com/kingstar/curso/api/controller/CozinhaController.java
+++ src/main/java/com/kingstar/curso/api/controller/CozinhaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kingstar.curso.api.controller.model.CozinhasXmlWrapper;
 import com.kingstar.curso.domain.entity.Cozinha;
 import com.kingstar.curso.domain.repository.CozinhaRepository;
 
@@ -22,6 +23,11 @@ public class CozinhaController {
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Cozinha> listar(){
 		return cozinhaRepository.listar();
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public CozinhasXmlWrapper listarXml(){
+		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
 	
 	@GetMapping("/{cozinhaId}")
