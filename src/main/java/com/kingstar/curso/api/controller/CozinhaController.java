afd6042f18e6fb94f05cca6a5f8e2888be14329b
pedrorhamon <pedrorhamon16@gmail.com>
2021-10-07 21:32:01 -0300

    manipulando respostas http

--- src/main/java/com/kingstar/curso/api/controller/CozinhaController.java
+++ src/main/java/com/kingstar/curso/api/controller/CozinhaController.java
@@ -3,7 +3,9 @@ package com.kingstar.curso.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,23 +18,25 @@ import com.kingstar.curso.domain.repository.CozinhaRepository;
 @RestController
 @RequestMapping(value = "/cozinhas")
 public class CozinhaController {
-	
+
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-	
+
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Cozinha> listar(){
+	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
 	}
-	
+
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public CozinhasXmlWrapper listarXml(){
+	public CozinhasXmlWrapper listarXml() {
 		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
-	
+
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId) {
-		return cozinhaRepository.buscar(cozinhaId);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
 	}
 
 }
