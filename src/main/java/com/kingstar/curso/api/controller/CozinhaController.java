b92d8d3dc1040bec58590c951072965621bc772a
pedrorhamon <pedrorhamon16@gmail.com>
2021-10-10 01:01:19 -0300

    status http e customizando respostas

--- src/main/java/com/kingstar/curso/api/controller/CozinhaController.java
+++ src/main/java/com/kingstar/curso/api/controller/CozinhaController.java
@@ -3,7 +3,6 @@ package com.kingstar.curso.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,7 +35,9 @@ public class CozinhaController {
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
 		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
 
-		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+		if (cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
+		return ResponseEntity.notFound().build();
 	}
-
 }
