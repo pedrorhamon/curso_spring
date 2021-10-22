68e8a2b704e78f9e2a9f9ead23cf95baf67137ed
pedrorhamon <pedrorhamon16@gmail.com>
2021-10-22 14:44:00 -0300

    criando arquivo orm.xml

--- src/main/java/com/kingstar/curso/domain/repository/RestauranteRepository.java
+++ src/main/java/com/kingstar/curso/domain/repository/RestauranteRepository.java
@@ -14,7 +14,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 
 	List<Restaurante> findByTaxaFrenteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
-	@Query("from Restaurante whrere nome like %:nome% and cozinha.id = :id")
+//	@Query("from Restaurante whrere nome like %:nome% and cozinha.id = :id")
 	List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
 
 	List<Restaurante> findByNomeContainingAndId(String nome, Long id);
