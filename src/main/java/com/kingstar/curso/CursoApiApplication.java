2860e63ac79f65d595fa6f76850ed26284d49c2d
pedrorhamon <pedrorhamon16@gmail.com>
2021-11-03 23:48:48 -0300

    CustomRepository

--- src/main/java/com/kingstar/curso/CursoApiApplication.java
+++ src/main/java/com/kingstar/curso/CursoApiApplication.java
@@ -2,8 +2,12 @@ package com.kingstar.curso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.kingstar.curso.infrastructure.repository.CustomJpaRepositoryImpl;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class CursoApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/kingstar/curso/domain/repository/RestauranteRepository.java
+++ src/main/java/com/kingstar/curso/domain/repository/RestauranteRepository.java
@@ -4,14 +4,13 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.Param;
 
 import com.kingstar.curso.domain.entity.Restaurante;
 
 public interface RestauranteRepository
-		extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
+		extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
 
 	List<Restaurante> findByTaxaFrenteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
