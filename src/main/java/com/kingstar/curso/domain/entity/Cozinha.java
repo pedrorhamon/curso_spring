a21731cefbb1533f250d713f25d15f80b4354e0b
pedrorhamon <pedrorhamon16@gmail.com>
2021-11-28 22:11:31 -0300

    Scema generation em producao

--- src/main/java/com/kingstar/curso/domain/entity/Cozinha.java
+++ src/main/java/com/kingstar/curso/domain/entity/Cozinha.java
@@ -30,6 +30,9 @@ public class Cozinha {
 	@Column(nullable = false)
 	private String nome;
 	
+	@Column(name = "observacao", nullable = false)
+	private String descricao;
+	
 	@JsonIgnore
 	@OneToMany(mappedBy = "cozinha")
 	private List<Restaurante> restaurantes = new ArrayList<>();
