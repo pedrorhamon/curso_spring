6e4e708b333dd48c81c5235e31a36853cc72571c
pedrorhamon <pedrorhamon16@gmail.com>
2021-12-16 19:32:08 -0300

    Lançando exceções do tipo ResponseStatusException

--- src/main/java/com/kingstar/curso/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/kingstar/curso/domain/service/CadastroCozinhaService.java
@@ -3,7 +3,9 @@ package com.kingstar.curso.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.kingstar.curso.domain.entity.Cozinha;
 import com.kingstar.curso.domain.exception.EntidadeEmUsoException;
@@ -24,8 +26,7 @@ public class CadastroCozinhaService {
 		try {
 			cozinhaRepository.deleteById(cozinhaId);
 		} catch (EmptyResultDataAccessException e) {
-			throw new EntidadeNaoEncontradaException(
-					String.format("Não existe um cadastro de cozinha no código %d ", cozinhaId));
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Não existe um cadastro de cozinha com código %d", cozinhaId));
 		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
 					String.format("Cozinha de código %d não pode ser removida, pois está em uso", cozinhaId));
