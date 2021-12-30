c63781c8f9e6668778cb2a72f8b230cec73ca1e9
pedrorhamon <pedrorhamon16@gmail.com>
2021-12-29 21:39:15 -0300

    Tratando exceções em nível de controlador com @ExceptionHandler

--- src/main/java/com/kingstar/curso/api/controller/CidadeController.java
+++ src/main/java/com/kingstar/curso/api/controller/CidadeController.java
@@ -1,9 +1,13 @@
 package com.kingstar.curso.api.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,8 +18,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kingstar.curso.domain.entity.Cidade;
+import com.kingstar.curso.domain.exception.EntidadeNaoEncontradaException;
 import com.kingstar.curso.domain.exception.EstadoNaoEncontradaException;
 import com.kingstar.curso.domain.exception.NegocioException;
+import com.kingstar.curso.domain.exception.exceptionhandler.Problema;
 import com.kingstar.curso.domain.repository.CidadeRepository;
 import com.kingstar.curso.domain.service.CadastroCidadeService;
 
@@ -59,5 +65,15 @@ public class CidadeController {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long cidadeId) {
 		cidadeService.excluir(cidadeId);
+	}
+	
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<?> tratarEstadoException(EntidadeNaoEncontradaException e){
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.msg(e.getMessage()).build();
+	
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(problema);
 	}
 }
