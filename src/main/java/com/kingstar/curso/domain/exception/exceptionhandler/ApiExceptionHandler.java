8ef354ed1ee1072ccfe9ea5b902288e0e8d29d63
pedrorhamon <pedrorhamon16@gmail.com>
2022-01-01 19:10:13 -0300

    Customizando o corpo da resposta padrão de ResponseEntityExceptionHandler

--- src/main/java/com/kingstar/curso/domain/exception/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/kingstar/curso/domain/exception/exceptionhandler/ApiExceptionHandler.java
@@ -2,55 +2,49 @@ package com.kingstar.curso.domain.exception.exceptionhandler;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.kingstar.curso.domain.exception.EntidadeNaoEncontradaException;
 import com.kingstar.curso.domain.exception.NegocioException;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 	
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEstadoException(EntidadeNaoEncontradaException e){
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.msg(e.getMessage()).build();
-	
-		return ResponseEntity.status(HttpStatus.NOT_FOUND)
-				.body(problema);
+	public ResponseEntity<?> tratarEstadoException(EntidadeNaoEncontradaException e, WebRequest resquest){
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, resquest);
 	}
 	
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarEstadoException(NegocioException e){
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.msg(e.getMessage()).build();
-	
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-				.body(problema);
+	public ResponseEntity<?> tratarEstadoException(NegocioException e, WebRequest resquest){
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, resquest);
 	}
-	
-	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-	public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException(){
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.msg("O Tipo de midia é em válido!").build();
-	
-		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-				.body(problema);	
-	}
-	
+
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> EntidadeNaoEncontradaException(){
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.msg("Ah um conflito").build();
+	public ResponseEntity<?> EntidadeNaoEncontradaException(EntidadeNaoEncontradaException e, WebRequest resquest){
+		return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, resquest);
+	}
 	
-		return ResponseEntity.status(HttpStatus.CONFLICT)
-				.body(problema);
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+		if (body == null) {
+			body = Problema.builder()
+					.dataHora(LocalDateTime.now())
+					.msg(status.getReasonPhrase())
+					.build();
+		}else if (body instanceof String) {
+			body = Problema.builder()
+					.dataHora(LocalDateTime.now())
+					.msg((String) body)
+					.build();
+		}
+		return new ResponseEntity<>(body, headers, status);
 	}
 }
