3f52e7b79317aa8641f7b3819d21bd826c220433
pedrorhamon <pedrorhamon16@gmail.com>
2022-01-11 20:10:51 -0300

    implementação de tratamento

--- src/main/java/com/kingstar/curso/api/controller/RestauranteController.java
+++ src/main/java/com/kingstar/curso/api/controller/RestauranteController.java
@@ -1,8 +1,17 @@
 package com.kingstar.curso.api.controller;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kingstar.curso.domain.entity.Restaurante;
 import com.kingstar.curso.domain.exception.EntidadeNaoEncontradaException;
 import com.kingstar.curso.domain.exception.NegocioException;
@@ -46,7 +57,7 @@ public class RestauranteController {
 	}
 
 	@PutMapping("/restaurantes/{id}")
-	public Restaurante atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante) {
+	public Restaurante atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante, HttpServletRequest requestR) {
 		Restaurante restauranteAtual = restauranteService.buscarFalha(restauranteId);
 		BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
 		try {
@@ -60,5 +71,27 @@ public class RestauranteController {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long restauranteId) {
 		restauranteService.excluir(restauranteId);
+	}
+	
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest requestR) {
+		ServletServerHttpRequest server = new ServletServerHttpRequest(requestR);
+		try {			
+			ObjectMapper object = new ObjectMapper();
+			object.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+			
+			Restaurante restauranteOrigem = object.convertValue(dadosOrigem, Restaurante.class);
+			
+			dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+				Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+				field.setAccessible(true);
+				
+				Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+				ReflectionUtils.setField(field, restauranteDestino, novoValor);
+				
+			});
+		}catch(IllegalArgumentException e) {
+			Throwable rootCause = ExceptionUtils.getRootCause(e);
+			throw new HttpMessageNotReadableException(e.getMessage(), rootCause, server);
+		}
 	}
 }
